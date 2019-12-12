bcc0f45dce2a6b152cd8b736a0056ab760549e27
shosy <ilikesgh22@gmail.com>
2019-12-12 11:32:52 +0900

    *4, /2への対応

--- chahan/outputAsm.ml
+++ chahan/outputAsm.ml
@@ -119,6 +119,12 @@ and output_exp oc exp =
   | Mul (x,y) -> op2 "Mul" x y
   | Div (x,y) -> op2 "Div" x y
   | SLL (x,y) -> op2' "SLL" x y
+  | SRA (x,y) ->
+      print_string "SRA ";
+      print_id x;
+      print_string " ";
+      print_int y;
+      print_string "\n"
   | Ld (x,y) -> op2' "Ld" x y
   | St (x,y,z) -> op3' "St" x y z
   | FMovD x -> op1 "FMovD" x
