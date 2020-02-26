e82b8b8742dbc0f669a444bde4828d953c2bceaf
shosy <ilikesgh22@gmail.com>
2020-02-26 23:13:41 +0900

    さらなる無駄なジャンプを消去

--- if.ml
+++ if.ml
@@ -1,6 +1,6 @@
 open KNormal
 
-let rec f = function
+(* let rec f = function
   (* 無駄なジャンプを消去 *)
   | Let((boo, Type.Int), IfEq(x, y, Int(0), Int(1)), Let((fal, Type.Int), Int(0), IfEq(boo', fal', e1, e2))) when boo = boo' && fal = fal' ->
       Let((boo, Type.Int), IfEq(x, y, Int(0), Int(1)), Let((fal, Type.Int), Int(0), IfEq(x, y, f e1, f e2)))
@@ -24,7 +24,7 @@ let rec f = function
   | Let(xt, e1, e2) -> Let(xt, f e1, f e2)
   | LetRec({ name = xt; args = yts; body = e1 }, e2) -> LetRec({ name = xt; args = yts; body = f e1 }, f e2)
   | LetTuple(xts, y, e) -> LetTuple(xts, y, f e)
-  | e -> e
+  | e -> e *)
 
 (* comment *)
 (* 共通部分式削除すると、こんな連続にデータが並ぶことはありえないか *)
@@ -33,19 +33,35 @@ let rec f = function
 (* let a = 1 in let rec f x = let b = 1 in x + b in f a が *)
 (* let a = 1 in let rec f x = x + a in f a とクロージャが必要になりそう *)
 
-(* let memif x env = 
-let mem0 x env = match M.find x env with Int(_) -> true | 
-
 let rec g env = function
-  | IfEq(x, y, e1, e2) when -> 
-      (match M.find x env with
-      | IfEq(z, w, e3, e4) when M.find y env = e3 -> IfEq(z, w, g env e1, g env e2)
-      | IfEq(z, w, e3, e4) when M.find y env = e4 -> IfEq(z, w, g env e1, g env e2)
-      | )
-  
-
-  | LetRec({ name = xt; args = yts; body = e1 }, e2) -> LetRec({ name = xt; args = yts; body = g env e1 }, g env e2)
-  | LetTuple(xts, y, e) -> LetTuple(xts, y, f e)
+  | IfEq(x, y, e1, e2) when M.mem x env && M.mem y env -> 
+      (match M.find x env, M.find y env with
+			| IfEq(z, w, Int(0), Int(1)), Int(0) -> IfEq(z, w, g env e1, g env e2)
+			| IfLE(z, w, Int(0), Int(1)), Int(0) -> IfLE(z, w, g env e1, g env e2)
+			| IfEq(z, w, Int(1), Int(0)), Int(0) -> IfEq(z, w, g env e2, g env e1)
+      | IfLE(z, w, Int(1), Int(0)), Int(0) -> IfLE(z, w, g env e2, g env e1)
+      | IfEq(z, w, Int(0), Int(1)), Int(1) -> IfEq(z, w, g env e2, g env e1)
+			| IfLE(z, w, Int(0), Int(1)), Int(1) -> IfLE(z, w, g env e2, g env e1)
+			| IfEq(z, w, Int(1), Int(0)), Int(1) -> IfEq(z, w, g env e1, g env e2)
+      | IfLE(z, w, Int(1), Int(0)), Int(1) -> IfLE(z, w, g env e1, g env e2)
+      | Int(0), IfEq(z, w, Int(0), Int(1)) -> IfEq(z, w, g env e1, g env e2)
+			| Int(0), IfLE(z, w, Int(0), Int(1)) -> IfLE(z, w, g env e1, g env e2)
+			| Int(0), IfEq(z, w, Int(1), Int(0)) -> IfEq(z, w, g env e2, g env e1)
+      | Int(0), IfLE(z, w, Int(1), Int(0)) -> IfLE(z, w, g env e2, g env e1)
+      | Int(1), IfEq(z, w, Int(0), Int(1)) -> IfEq(z, w, g env e2, g env e1)
+			| Int(1), IfLE(z, w, Int(0), Int(1)) -> IfLE(z, w, g env e2, g env e1)
+			| Int(1), IfEq(z, w, Int(1), Int(0)) -> IfEq(z, w, g env e1, g env e2)
+      | Int(1), IfLE(z, w, Int(1), Int(0)) -> IfLE(z, w, g env e1, g env e2)
+      | _ -> IfEq(x, y, g env e1, g env e2))
+  | IfEq(x, y, e1, e2) -> IfEq(x, y, g env e1, g env e2)
+  | IfLE(x, y, e1, e2) -> IfLE(x, y, g env e1, g env e2)
+  | Let((x, t), e1, e2) -> 
+      let e1' = g env e1 in
+      Let((x, t), e1', g (M.add x e1' env) e2)
+  | LetRec({ name = xt; args = yts; body = e1 }, e2) -> 
+      LetRec({ name = xt; args = yts; body = g env e1 }, g env e2)
+  | LetTuple(xts, y, e) -> 
+      LetTuple(xts, y, g env e)
   | e -> e
 
-let f e = g M.empty e *)
+let f e = g M.empty e
--- 今の所関係ない/subexp.ml
+++ 今の所関係ない/subexp.ml
@@ -1,5 +1,41 @@
 open Asm
 
+let rec rename x x' = function
+  | Ans(exp) -> Ans(rename' x x' exp)
+  | Let((y, t), exp, e) -> 
+      Let((y, t))
+let rec rename' x x' exp =
+  let re y = if y = x then x' else y in
+  let ri = function V(y) -> V(re y) | C(i) -> C(i) in
+  let rf = function W(y) -> W(re y) | D(f) -> D(f) in
+  match exp with
+  | Nop -> Nop
+  | Set(i) -> Set(i)
+  | FSetD(f) -> FSetD(f)
+  | SetL(l) -> SetL(l)
+  | Mov(y) -> Mov(re y)
+  | Add(y, z) -> Add(ri y, ri z)
+  | Addi(y, i) -> Addi(ri y, i)
+  | Sub(y, z) -> Sub(ri y, ri z)
+  | Mul(y, z) -> Mul(ri y, ri z)
+  | Div(y, z) -> Div(ri y, ri z)
+  | SLL(y, i) -> SLL(ri y, i)
+  | SRA(y, i) -> SRA(ri y, i)
+  | Ld(y, i) -> Ld(ri y, i)
+  | St(y, z, i) -> St(ri y, ri z, i)
+  | FMovD(y) -> FMovD(rf y)
+  | FNegD(y) -> FNegD(rf y)
+  | FAddD(y, z) -> FAddD(rf y, rf z)
+  | FSubD(y, z) -> FSubD(rf y, rf z)
+  | FMulD(y, z) -> FMulD(rf y, rf z)
+  | FInv(y) -> FInv(rf y)
+  | LdDF(y, i) -> LdDF(ri y, i)
+  | StDF(y, z, i) -> StDF(rf y, ri z, i)
+  | IfEq(y, z, e1, e2) -> 
+  | IfLE(y, z, e1, e2) -> 
+  | IfFEq(y, z, e1, e2) ->
+  | IfFLE(y, z, e1, e2) -> 
+
 let rec g env =
   | Let((x, t), exp, e) ->
       let exp' = g' env exp in
@@ -12,3 +48,27 @@ let rec g env =
         Let((x, t), Mov(find (Add(y, z) env)), g env e)
       else
         let env' = M.add (Add(z, y)) 
+
+and g' = function
+  | Add()
+
+let rec g env =
+  | Let((x, t), exp, e) ->
+      let exp' = g' env exp in
+      try 
+        let y = List.assoc exp' env in
+        (comment;
+        )
+      with Not_Found -> 
+        Let((x, t), exp', g  e)
+
+and g' env = function
+  | Add(x, y) as exp -> exp, [Add(x, y); Add(y, x)]
+  | Addi(x, i) as exp -> exp, [Addi(x, i)]
+  | Sub(x, y) as exp -> exp, [Sub(x, y)]
+  | Mul(x, y) as exp -> exp, [Mul(x, y); Mul(y, x)]
+  | Div(x, y) as exp -> exp, [Div(x, y)]
+  | SLL(x, i) as exp -> exp, [SLL(x, i)]
+  | SRA(x, i) as exp -> exp, [SRA(x, i)]
+  | Ld(x, i) as exp -> exp, [Ld(x, i)]
+  
